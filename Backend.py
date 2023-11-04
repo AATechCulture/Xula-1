c7f8747f08a7070aaa31c10da77608dd7b48bba8
Kaleb Riley <110492034+Kale13-R@users.noreply.github.com>
2023-11-04 14:19:49 -0500

    Update Backend.py

--- Backend.py
+++ Backend.py
@@ -14,8 +14,21 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+# user prefernces will be in the form 
+# pref = {temp pref: [True,False,True,Flase]
+#         time_of_yr: [True,True,False,False
+#         Dairy:
+#         Gluten:
+#         Shellfish:
+#         Nuts:
+#         halaal:
+#         Walkable: bool
+#         region: [bool, bool,bool, bool,bool]
+#         kid friendly: bool
+#         nightlife: bool}
+                     
 def detail_information(location):
     return dict of matches
 
-def trip_match(dict):
+def trip_match(dict,user_preferences):
     return 

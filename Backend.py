1ab6986178f2a3aa50d2bbef8fa95f390b98b215
Kaleb Riley <110492034+Kale13-R@users.noreply.github.com>
2023-11-04 12:41:54 -0500

    Update Backend.py

--- Backend.py
+++ Backend.py
@@ -13,3 +13,9 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+def detail_information(location):
+    return dict of matches
+
+def trip_match(dict):
+    return 

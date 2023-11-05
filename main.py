bb3c3cafb9af1352d94ad3d68ace6d1d01febdb0
Chioma Ifezie <chiomaifezie@Chiomas-MacBook-Air.local>
2023-11-04 19:50:38 -0500

    Server for frontend

--- main.py
+++ main.py
@@ -1,29 +1,49 @@
-from typing import Union
+# from typing import Union
 from fastapi import FastAPI
+import json
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 import requests
+import uvicorn
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "*",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
 
+class Data(BaseModel):
+    data: dict
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return main()
-
+@app.post("/test")
+async def read_item(data:Data):
+    user = 'test'
+    # user = data.user
+    f = open(user + ".txt", "w")
+    f.write(json.dumps(data.data))
+    f.close()
+    return data.data
     
 def main():
     url = 'https://ef75-144-9-80-252.ngrok.io/flights?date=2020-01-01'
     print("Hello World!")
-    data = requests.request("GET", url)
-    print(data.text)
+    # data = requests.request("GET", url)
+    # print(data.text)
 
 
 
 
 
 if __name__ == '__main__':
-    main()
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
